8d1fa3b31961138fa8a66cf4cd92d21e54cfe96c
Edwin Becerra <edwin.becerra@uniminuto.edu.co>
2021-06-20 19:14:21 -0500

    Create rot13.py
    Create the file and the function rot13

---
+++ rot13.py
@@ -0,0 +1,41 @@
+def myRot(text, n):
+	'''
+	This function receives two parameters, the text to be encrypted and 
+	the rotation number of the alphabet
+	'''
+	upperAlph = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
+				'P','Q','R','S','T','U','V','W','X','Y','Z']
+	lowerAlph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+				'p','q','r','s','t','u','v','w','x','y','z']
+
+	upperRotation = upperAlph[:]
+	lowerRotation = lowerAlph[:]
+
+	textEncrypt = ''
+	
+
+	for i in range(0, n):
+		upperLetter = upperRotation.pop(0)
+		lowerLetter = lowerRotation.pop(0)
+		upperRotation.append(upperLetter)
+		lowerRotation.append(lowerLetter)
+
+	for letter in text:
+		rotationNum = 0
+		for i in upperAlph:
+			if letter == i:
+				textEncrypt += upperRotation[rotationNum]
+			rotationNum += 1
+
+		rotationNum = 0
+		for i in lowerAlph:
+			if letter == i:
+				textEncrypt += lowerRotation[rotationNum]
+			rotationNum += 1
+
+		if not letter in upperAlph and not letter in lowerAlph:
+			textEncrypt += letter
+
+	return textEncrypt
+
+print(myRot('Hola12 Mundo321', 13))
